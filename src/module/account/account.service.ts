d077d135f169c6c4a9b837813d413bdff229368f
Luan Fonseca <luanfonsecap@icloud.com>
2021-06-02 15:43:06 -0300

    created endpoint and validations to create user

--- src/module/account/account.service.ts
+++ src/module/account/account.service.ts
@@ -1,8 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  Inject,
+  Logger,
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+
+import { CreateUserDTO } from './dtos/CreateUser.dto';
+import { AccountServiceConfig } from '../../config/microservices.config';
 
 @Injectable()
 export class AccountService {
-  create(): string {
-    return 'account created!';
+  private logger = new Logger();
+
+  constructor(
+    @Inject(AccountServiceConfig.name)
+    private readonly accountService: ClientProxy,
+  ) {}
+
+  async create(data: CreateUserDTO) {
+    return this.accountService
+      .send('createUser', data)
+      .toPromise()
+      .catch((err) => {
+        this.logger.error(err);
+        if (err.httpCode === 400) {
+          throw new BadRequestException(err.message);
+        }
+        throw new InternalServerErrorException();
+      });
   }
 }
