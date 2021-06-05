bef76104b62e3e6bd6ae1b2d01a817ce399cfc8b
Hermes Francisco <franciscohermes2000@hotmail.com>
2021-06-04 23:22:57 -0300

    Error code

--- src/module/cipher/cipher.controller.ts
+++ src/module/cipher/cipher.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { CipherService } from './cipher.service';
 import { LoginDTO } from './dtos/Login.dto';
 
@@ -7,6 +7,7 @@ export class CipherController {
   constructor(private readonly cipherService: CipherService) {}
 
   @Post()
+  @HttpCode(200)
   login(@Body() request: LoginDTO) {
     return this.cipherService.login(request);
   }
