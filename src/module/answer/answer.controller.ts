f89ae71d835a7482c2831180133ec8cf74a3923c
Luan Fonseca <luanfonsecap@icloud.com>
2021-06-13 19:35:56 -0300

    fix controller and service create answer

--- src/module/answer/answer.controller.ts
+++ src/module/answer/answer.controller.ts
@@ -1,28 +1,14 @@
-import {
-  Body,
-  Controller,
-  Post,
-  Request,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Body, Controller, Post, Request } from '@nestjs/common';
+
 import { CreateAnswerDTO } from './dtos/CreateAnswer.dto';
 import { AnswerService } from './answer.service';
 
 @Controller('answer')
 export class AnswerController {
   constructor(private readonly answerService: AnswerService) {}
+
   @Post()
   create(@Request() req, @Body() data: CreateAnswerDTO) {
-    if (req.user.id !== data.accountId)
-      throw new UnauthorizedException(
-        'You can not create a Answer for other user',
-      );
-    
-    /*if (req.user.role !== "LAWYER")
-      throw new UnauthorizedException(
-        'You can not create a Answer',
-      );*/
-
-    return this.answerService.create(data);
+    return this.answerService.create(data, req.user.id);
   }
 }
